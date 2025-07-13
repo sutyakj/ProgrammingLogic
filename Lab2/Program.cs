a597fdb2b7bb4f8a31dcf928ef7c5bd48be2c136
sutyakj <sutyakjeffrey@gmail.com>
2025-07-13 22:18:04 +0000

    Committing Lab 2 Changes up to step 3

---
+++ Lab2/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Hello, World!");
+
+//Part 1- Operator Practice
+
+int a = 10; int b = 5; int c = 15;
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+
+// comparison
+
+bool isAGreaterThanB = a > b;
+bool isALessThanC = a < c;
+bool isAGreaterThanBandC = (a > b) && (a > c);
+
+bool isAGreaterThanBorC = (a > b) || (a > c);
+
+Console.WriteLine("a >b:" + isAGreaterThanB);
+Console.WriteLine("a < c:" + isALessThanC);
+Console.WriteLine("a > b && a > c:" + isAGreaterThanBandC);
+Console.WriteLine("a > b || a > c:" + isAGreaterThanBorC);
+
+//Part 2 Boolean Logic
+bool isRaining = true;
+bool haveUmbrella = false;
+
+if (isRaining && !haveUmbrella)
+{
+    Console.WriteLine("Take an umbrella!");
+}
+else
+{
+    Console.WriteLine("You're good to go!");
+}
+
