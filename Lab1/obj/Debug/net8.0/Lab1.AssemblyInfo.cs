ea142f77ce55fbd86c53874baf8d1face538e46d
sutyakj <sutyakjeffrey@gmail.com>
2025-07-13 22:36:50 +0000

    Re-Committing after entering values in terminal

--- Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
+++ Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52e04b308f08edf57fea8ce1abd6ea21f9212b91")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15a03f0657767baa1f42bf7dbf3d59d0b5d80fd8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
