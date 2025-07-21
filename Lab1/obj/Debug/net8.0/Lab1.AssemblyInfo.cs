1ce6b42befe6736892bacaf4db3e1cd8e080c483
sutyakj <sutyakjeffrey@gmail.com>
2025-07-21 21:15:02 +0000

    Commit Final Changes to Lab 4

--- Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
+++ Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fff8cda041a27a2211d8fb561bfb5d92ef2ddde2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4749c312491d57bff566fcb7107eae58e9cf392")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Lab4/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Lab4
+{
+    // Define the Car class
+    class Car
+    {
+        public string model;
+        public string color;
+        public int year;
+
+        // Method to display car details
+        public void Display()
+        {
+            Console.WriteLine("Car Details:");
+            Console.WriteLine("Model: " + model);
+            Console.WriteLine("Color: " + color);
+            Console.WriteLine("Year: " + year);
+        }
+        // Problem 1: Add Start method
+        public void Start()
+        {
+            Console.WriteLine("The car is starting.");
+        }
+
+        // Problem 2: Drive method with a parameter
+        public void Drive(int miles)
+        {
+            Console.WriteLine($"The car drove {miles} miles.");
+        }
+
+        // Problem 3: Method with return value
+        public string GetDescription()
+        {
+            return $"{year} {color} {model}";
+        }
+        
+        // Problem 4: Repaint method
+        public void Repaint(string newColor)
+        {
+            color = newColor;
+            Console.WriteLine($"The car has been repainted to {newColor}.");
+        }        
+    }
+
+    // Main Program class
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            // Creating a Car object and assigning values to its properties
+            Car myCar = new Car();
+            myCar.model = "Civic";
+            myCar.color = "Black";
+            myCar.year = 2020;
+
+            myCar.Display(); // Calling the Display method
+            myCar.Start(); // Call the start method
+            myCar.Drive(50); //Call the Drive method with 50 miles
+
+            //Problem 3: Get and Print Description
+            string description = myCar.GetDescription();
+            Console.WriteLine("Description:" + description);
+
+            //Problem 4: Repaint the car
+            myCar.Repaint("Red");
+
+            //Confirm color update
+            Console.WriteLine("Updated Description: " + myCar.GetDescription());
+        }
+    }
+}
