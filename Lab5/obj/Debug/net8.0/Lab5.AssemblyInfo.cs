a11f20df90ed5a1f1e68de145a9eddd1d16254ce
sutyakj <sutyakjeffrey@gmail.com>
2025-07-28 19:45:47 +0000

    Final Commit Lab 5

--- Lab5/obj/Debug/net8.0/Lab5.AssemblyInfo.cs
+++ Lab5/obj/Debug/net8.0/Lab5.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d41c3df618ba59fadf7b98887c88020e840a3572")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44a29f6c72f7b12dc62bd4bb404866ef7652774e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
